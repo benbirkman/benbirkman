633f705851bd1fc22cb5ac090b8051301ad38e39
benbirkman <33761252+benbirkman@users.noreply.github.com>
2017-11-17 20:28:16 +0200

    first version

---
+++ 1.js
@@ -0,0 +1,123 @@
+function randomNumber(a, b) {
+
+	return Math.floor(Math.random() * (b - a + 1)) + a;
+}
+
+function randomChar(str) {
+
+	n = randomNumber(0, str.length - 1);
+	return str[n];
+}
+
+function randomOperation() {
+	
+	return randomChar("/*^+-");
+}
+
+function factorial(n) {
+	result = 1;
+	
+	for (i = 1; i <= n; i++) { 
+		result = result * i;
+	}
+	
+	return result;
+}
+
+function abs(n) {
+	
+	if (n < 0) {
+		return -n;
+	} else {
+		return n;
+	}
+}
+
+function exponent(n, m) {
+	
+	result = 1;
+	
+	for (i = 0; i < abs(m); i++) {
+		result = result * n;
+	}
+	
+	if (m < 0) {
+		
+		return 1 / result;
+	} else {
+		return result;
+	}
+}
+ 
+function calculate(a, b, operation) {
+
+	
+	if (operation == "+") {
+		result = a + b;
+	}
+	
+	if (operation == "*") {
+		result = a * b;
+	}
+
+	if (operation == "/") {
+		result = a / b;
+	}
+	
+	if (operation == "-") {
+		result = a - b;
+	}
+	
+	if (operation == "^") {
+		result = exponent(a, b);
+	}
+	
+	return result;
+}
+
+function calculator() {
+			
+	operation = document.getElementById("operation").value;
+	a = Number(document.getElementById("a").value);
+	b = Number(document.getElementById("b").value);
+	
+	document.getElementById("result").value = calculate(a, b, operation);
+}
+
+function randomExercise() {
+	
+	operation = randomOperation();
+	
+	if (operation == "^") {
+		min = 2;
+		max = 16;
+	} else {
+		min = -50000;
+		max = -min;
+	}
+	
+	random1 = randomNumber(min, max);
+	random2 = randomNumber(min, max);
+	
+	document.getElementById("n1").innerHTML = random1;	
+	document.getElementById("n2").innerHTML = random2;		
+	document.getElementById("operation").innerHTML = operation;			
+}
+
+function checkResult() {
+	
+	result1 = document.getElementById("result").value;
+	a = Number(document.getElementById("n1").innerHTML);
+	b = Number(document.getElementById("n2").innerHTML); 
+	operation = document.getElementById("operation").innerHTML;
+	
+	result2 = calculate(a, b, operation);
+	
+	if (result1 == result2) {
+		document.getElementById("success").innerHTML = "true";			
+	} else {
+		document.getElementById("success").innerHTML = "false";			
+	}
+}
+
+
