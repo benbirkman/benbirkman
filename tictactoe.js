349dacbe9789f41d0a67405df640df0b1a0f339b
benbirkman <33761252+benbirkman@users.noreply.github.com>
2017-11-27 21:39:38 +0200

    wip

--- tictactoe.js
+++ tictactoe.js
@@ -1,16 +1,43 @@
 
 firstPlayer = true;
- 
-function ticTac(elm) {
+cells = [];
 
-	if (firstPlayer) {
-		elm.innerHTML = "X";
-		
-	} else {
-		elm.innerHTML = "O";
+function checkWin() {
+	tor = cells["tl"] != null && cells["tl"] == cells["tm"] && cells["tm"] == cells["tr"];
+	mor = cells["ml"] != null && cells["ml"] == cells["mm"] && cells["mm"] == cells["mr"];
+	bor = cells["bl"] != null && cells["bl"] == cells["bm"] && cells["bm"] == cells["br"];
+	lc = cells["br"] != null && cells["br"] == cells["mr"] && cells["mr"] == cells["tr"];
+	mc = cells["bm"] != null && cells["bm"] == cells["mm"] && cells["mm"] == cells["tm"];
+	rc = cells["bl"] != null && cells["bl"] == cells["ml"] && cells["ml"] == cells["tl"];
+	db = cells["tl"] != null && cells["tl"] == cells["mm"] && cells["mm"] == cells["br"];
+	dt = cells["bl"] != null && cells["bl"] == cells["mm"] && cells["mm"] == cells["tr"];
+
+	return tor || mor || bor || lc || mc || rc || db || dt;
+}
+ 
+function ticTac(elm, location) {
+	if (cells[location] != null) {
+		return;
 	}
 	
-	firstPlayer = !firstPlayer;
-	elm.onclick = null;	
 
+	if (firstPlayer) {
+		sign = "X";
+		otherSign = "O"
+		
+	} else {
+		sign = "O";
+		otherSign = "X"
+	}
+	
+	cells[location] = sign;
+	elm.innerHTML = sign;
+	
+	
+	firstPlayer = !firstPlayer;
+	
+	if (checkWin()) {
+		document.getElementById("result").innerHTML = sign + " wins!!! " + otherSign + " is a total loser";
+	}
+	
 }
